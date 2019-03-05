9c34fbc7274cf572ea2f77c3743f0da90b292ac8
IvanPilyak <pilyak.vanya@gmail.com>
2019-03-05 05:13:32 +0200

    Added carusel.

--- src/js/move_progress_bar.js
+++ src/js/move_progress_bar.js
@@ -1,14 +1,23 @@
-function move() {
-    var elem = document.getElementById("progres-bar"); 
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-        elem.innerHTML.width = width * 1 + '%';
-      }
-    }
-  }
+
+  $(document).ready(function() {
+
+    $('.carousel').slick({
+      autoplay: true,
+      dots: true,
+      slidesToShow: 4,
+      speed: 500,
+      autoplaySpeed: 2000
+    });
+  });
+
+
+  $('.progress-bar').each(function() {
+
+    var valueNow = $(this).attr('aria-valuenow');
+    $(this).animate({
+      
+      width: valueNow + '%',
+    }, 2500);
+  });
+
+
